f27a7b00a7e9e6590ae362f5f7c881f6eba174f7
myf5 <mapdof@126.com>
2022-06-08 19:01:48 +0800

    build dev 0.5.3.2

--- pkg/as3/register.go
+++ pkg/as3/register.go
@@ -5,16 +5,17 @@ import "sync"
 var syncMap sync.Map
 
 const (
-	as3DefaultTemplateKey   = "__AS3_DEFAULT_TEMPLATE__"
-	currentClusterKey       = "__CLUSTER__"
-	isSupportRouteDomainKey = "__IS_SUPPORT_ROUTE_DOMAIN__"
-	logPoolKey              = "__LOG_POOL__"
-	schemaVersionKey        = "__SCHEMAVERSION__"
-	namespaceCacheKey       = "__NAMESPACE_CACHE_KEY__"
-	partitionCacheKey       = "__PARTITION_CACHE_KEY__"
-	masterClusterKey        = "__MASTER_CLUSTER__"
-	as3IRulesListKey        = "__AS3_IRULES_LIST_KEY__"
-	clusterSvcExtNamespaceKey         = "__CLUSTER_SVC_EXT_NAMESPACE__"
+	as3DefaultTemplateKey     = "__AS3_DEFAULT_TEMPLATE__"
+	currentClusterKey         = "__CLUSTER__"
+	isSupportRouteDomainKey   = "__IS_SUPPORT_ROUTE_DOMAIN__"
+	logPoolKey                = "__LOG_POOL__"
+	schemaVersionKey          = "__SCHEMAVERSION__"
+	cniTypeKey                = "__CNITYPE__"
+	namespaceCacheKey         = "__NAMESPACE_CACHE_KEY__"
+	partitionCacheKey         = "__PARTITION_CACHE_KEY__"
+	masterClusterKey          = "__MASTER_CLUSTER__"
+	as3IRulesListKey          = "__AS3_IRULES_LIST_KEY__"
+	clusterSvcExtNamespaceKey = "__CLUSTER_SVC_EXT_NAMESPACE__"
 )
 
 func registValue(name, v interface{}) {
